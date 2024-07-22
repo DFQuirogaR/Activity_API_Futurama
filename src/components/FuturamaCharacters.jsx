7d071c8d533a1892f545626a9a723fdcfc8ba037
Diego Quiroga <113153053+DFQuirogaR@users.noreply.github.com>
2024-07-21 20:52:16 -0500

    Finished style

--- src/components/FuturamaCharacters.jsx
+++ src/components/FuturamaCharacters.jsx
@@ -18,21 +18,21 @@ export default function App() {
 
     return (
         <div>
-            <div className="p-3 text-primary-emphasis bg-primary-subtle border border-primary-subtle rounded-3">
+            <div className="p-3 bg-secondary text-white border border-primary-subtle rounded-3">
                 <h1>Futurama Characters</h1>
             </div>
             <div className='container'>
                 <div className='row d-flex flex-wrap row-cols-1 row-cols-md-2 row-cols-lg-3'>
                     {data.map((item, index) => {
                         return (
-                            <div className="card" style={{ width: "18rem;" }}>
-                                <img src={item.images.main} className="card-img-top" alt="..." style={{ height: '100px', objectFit: 'contain' }} />
+                            <div className="card bg-primary-subtle text-primary-emphasis border border-light" style={{ width: "18rem;" }}>
+                                <img src={item.images.main} className="card-img-top" alt="..." style={{ height: '200px', objectFit: 'contain' }} />
                                 <div className="card-body">
                                     <h5 className="card-title">{item.name.first} {item.name.middle} {item.name.last}</h5>
-                                    <p className="card-text">{item.age}</p>
-                                    <p className="card-text">{item.gender}</p>
-                                    <p className="card-text">{item.species}</p>
-                                    <p className="card-text">{item.occupation}</p>
+                                    <p className="card-text">Age: {item.age}</p>
+                                    <p className="card-text">Gender: {item.gender}</p>
+                                    <p className="card-text">Species: {item.species}</p>
+                                    <p className="card-text">Occupation:  {item.occupation}</p>
                                 </div>
                             </div>
                         )
